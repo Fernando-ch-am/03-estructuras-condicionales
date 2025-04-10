b807d3adf688558974c5340a57d3c4ac108724f9
Fernano-ch-am <fernandochaconamati@gmail.com>
2025-04-10 00:13:15 -0300

    punto1.tp4

--- trabajo 3/punto3.py
+++ trabajo 3/punto3.py
@@ -2,8 +2,7 @@
 #número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso
 #contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del
 #operador de módulo (%) en Python para evaluar si un número es par o impar.
-
-num=input("ingrese un numero par: ")
+num=input("ingrese un numero: ")
 num=int(num)
 if(num % 2 == 0):
     print("Ha ingresado un número par")
---
+++ trabajo 4/punto1-tp4.py
@@ -0,0 +1,6 @@
+#1) Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100
+#(incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
+
+cont=1
+for cont in range(1,100+1):
+    print(cont)
