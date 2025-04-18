cb4cb947bcb177c26a629dff41de163ab4ede311
Fernano-ch-am <fernandochaconamati@gmail.com>
2025-04-17 23:19:59 -0300

    punto 1

---
+++ trabajo 5/punto1.py
@@ -0,0 +1,7 @@
+#1. Crear una función llamada imprimir_hola_mundo que imprima por
+#pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
+#programa principal.
+def imprimir_hola_mundo(mensaje):
+    return print("Hola Mundo!")
+mensaje=0
+imprimir_hola_mundo(mensaje)
