5e1ee7d112090a0177083372d3a69e2e6acdc1c1
Fernano-ch-am <fernandochaconamati@gmail.com>
2025-04-26 17:34:41 -0300

    punto 3

---
+++ trabajo 6/punto3.py
@@ -0,0 +1,9 @@
+#3) Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por
+#pantalla. Pista: para crear una lista vacía debes colocar los corchetes sin nada en su interior. Por
+#ejemplo:
+
+lista=[]
+lista.append("hola")
+lista.append("como")
+lista.append("estas")
+print(lista)
