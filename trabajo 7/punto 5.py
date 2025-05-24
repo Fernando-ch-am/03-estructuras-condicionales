2a9aa32adb113ff45a7b9beef04582c2639e17f5
Fernano-ch-am <fernandochaconamati@gmail.com>
2025-05-23 21:26:22 -0300

    punto 5

---
+++ trabajo 7/punto 5.py
@@ -0,0 +1,10 @@
+palabra=input("ingrese una palabra: ")
+def es_palindromo(palabra):
+    
+    if len(palabra) <= 1:
+        return True
+    if palabra[0] != palabra[-1]:
+        return False
+    return es_palindromo(palabra[1:-1])
+
+print(es_palindromo(palabra))
