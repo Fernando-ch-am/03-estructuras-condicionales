4725e51317e59b618cce88e7b519f609640e90b2
Fernano-ch-am <fernandochaconamati@gmail.com>
2025-06-13 00:39:04 -0300

    punto 3

---
+++ trabajo 8/punto 3.py
@@ -0,0 +1,8 @@
+#3) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código
+#desarrollado en el punto anterior, crear una lista que contenga únicamente las frutas sin los
+#precios.
+
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melon': 3000, 'Uva':1450}
+
+precios_frutas=list(precios_frutas.keys())
+print(precios_frutas)
